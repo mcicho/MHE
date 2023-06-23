c15a4bd207b52553e446ac83b3a5b2c10d6159d2
MC <s16512@pjwstk.edu.pl>
2023-06-24 01:29:13 +0200

    Experiments part2

--- experiment.sh
+++ experiment.sh
@@ -6,12 +6,12 @@ P_COOL="0.5 0.7 0.9"
 
 rm -f results/results3.txt
 for i in `seq 1 $REPEATS`; do
-for temp in $P_TEMP; do
-for cool in $P_COOL; do
 rhc=`python random_hill_climbing.py 100`
 hc=`python hill_climbing.py 100`
 ts=`python tabu_search.py 100 5`
 tsb=`python tabu_search_backtrack.py 100 5`
+for temp in $P_TEMP; do
+for cool in $P_COOL; do
 sa=`python simulated_annealing.py 100 5 $temp $cool`
 
 echo "random_hill_climbing $rhc" >> results/results3.txt
